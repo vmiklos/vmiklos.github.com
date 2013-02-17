f9b4ce39babfebe4002b5caf51695fad8d631c69
Miklos Vajna <vmiklos@vmiklos.hu>
2013-02-17 12:23:06 +0100

    add my visited countries

---
+++ visited-countries/countries.js
@@ -0,0 +1,17 @@
+countries = [
+	'austria', // Murau 2005, Wien 2010
+	'belgium', // Brussels 2013
+	'croatia', // Poreč 2011, Plitvice Lakes 2012
+	'czech', // Prague 2010, Prague 2012
+	'france', // Isola 2009, Isola 2011, Paris 2012
+	'germany', // Nurnberg 2010, Munich 2011, Hamburg 2012, Berlin 2012, Munich 2012
+	'greece', // Rhodes 2012
+	'hungary',
+	'italy', // Rome 2009
+	'monaco', // Monaco 2011
+	'poland', // Poznan 2010
+	'romania',
+	'slovakia',
+	'slovenia', // Koper 2011
+	'turkey', // Marmaris 2012
+]
