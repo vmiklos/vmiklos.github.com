10f04376a20a524cd4eaec8b592fcbd896cc6705
Miklos Vajna <vmiklos@vmiklos.hu>
2013-03-16 21:39:34 +0100

    visited-countries: add bosnia

--- visited-countries/countries.js
+++ visited-countries/countries.js
@@ -1,6 +1,7 @@
 countries = [
 	'austria', // Murau 2005, Wien 2010
 	'belgium', // Brussels 2013
+	'bosnia', // Jahorina 2013
 	'croatia', // Poreč 2011, Plitvice Lakes 2012
 	'czech', // Prague 2010, Prague 2012
 	'france', // Isola 2009, Isola 2011, Paris 2012
@@ -10,8 +11,8 @@ countries = [
 	'italy', // Rome 2009
 	'monaco', // Monaco 2011
 	'poland', // Poznan 2010
-	'romania',
-	'slovakia',
+	'romania', // Cluj-Napoca, etc.
+	'slovakia', // Krompachy, etc.
 	'slovenia', // Koper 2011
 	'turkey', // Marmaris 2012
 ]
