ee5281f6d853cec224e5624b3661e4852744a45b
Miklos Vajna <vmiklos@vmiklos.hu>
2015-05-15 22:15:26 +0200

    visited-countries: add .es

--- visited-countries/countries.js
+++ visited-countries/countries.js
@@ -1,10 +1,11 @@
 countries = [
 	'at', // Austria: Murau 2005, Wien 2010
 	'ba', // Bosnia and Herzegovina: Jahorina 2013
-	'be', // Belgium: Brussels 2013, Brussels 2014
+	'be', // Belgium: Brussels 2013, Brussels 2014, Brussels 2015
 	'ch', // Switzerland: Bern 2014
 	'cz', // Czech Republic: Prague 2010, Prague 2012
 	'de', // Germany: Nurnberg 2010, Munich 2011, Hamburg 2012, Berlin 2012, Munich 2012
+	'es', // Spain: Zaragoza 2015
 	'fr', // France: Isola 2009, Isola 2011, Paris 2011
 	'gr', // Greece: Rhodes 2012
 	'hr', // Croatia: Poreč 2011, Plitvice Lakes 2012
