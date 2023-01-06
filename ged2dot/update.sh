d29c861bfff99783b8dfa6fb2469f867f3be22d6
Miklos Vajna <vmiklos@vmiklos.hu>
2023-01-06 21:09:08 +0100

    ged2dot: update from git

--- ged2dot/update.sh
+++ ged2dot/update.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 #
-# Copyright 2020 Miklos Vajna. All rights reserved.
+# Copyright 2022 Miklos Vajna. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 #
 
-cat ~/git/ged2dot/README.adoc > index.adoc
-echo >> index.adoc
-echo "include::../matomo.adoc[]" >> index.adoc
-make
+cp ~/git/ged2dot/libreoffice/screenshot.png libreoffice/
+cp ~/git/ged2dot/tests/screenshot.png tests/
+rsync --exclude '*.swp' -avP ~/git/ged2dot/guide/book/ .
 git add .
 git commit -m "ged2dot: update from git"
 
