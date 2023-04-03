cf8788c8af6b0a4ed4c85d8f9b14294519372e41
Miklos Vajna <vmiklos@vmiklos.hu>
2023-04-03 09:27:14 +0200

    Update from https://github.com/glauberramos/visited-countries.git

--- visited-countries/countries.js
+++ visited-countries/countries.js
@@ -1,24 +1,26 @@
 countries = [
-	'al', // Albania: Tirana 2018
-	'at', // Austria: Murau 2005, Wien 2010
-	'ba', // Bosnia and Herzegovina: Jahorina 2013
-	'be', // Belgium: Brussels 2013, Brussels 2014, Brussels 2015
-	'ch', // Switzerland: Bern 2014
-	'cz', // Czech Republic: Prague 2010, Prague 2012
-	'de', // Germany: Nurnberg 2010, Munich 2011, Hamburg 2012, Berlin 2012, Munich 2012
-	'dk', // Denmark: Aarhus 2015
-	'es', // Spain: Zaragoza 2015
-	'fr', // France: Isola 2009, Isola 2011, Paris 2011
-	'gb', // United Kingdom: Cambridge 2015
-	'gr', // Greece: Rhodes 2012
-	'hr', // Croatia: Poreč 2011, Plitvice Lakes 2012
-	'hu', // Hungary
-	'it', // Italy: Rome 2009, Milan 2013
-	'mc', // Monaco: 2011
-	'pl', // Poland: Poznan 2010
-	'ro', // Romania: Cluj-Napoca, etc.
-	'se', // Sweden: Malmo 2015
-	'si', // Slovenia: Koper 2011
-	'sk', // Slovakia: Krompachy, etc.
-	'tr', // Turkey: Marmaris 2012
-]
+	'albania', // Albania: Tirana 2018
+	'austria', // Austria: Murau 2005, Wien 2010
+	'bosnia', // Bosnia and Herzegovina: Jahorina 2013
+	'belgium', // Belgium: Brussels 2013, Brussels 2014, Brussels 2015
+	'switzerland', // Switzerland: Bern 2014
+	'czech', // Czech Republic: Prague 2010, Prague 2012
+	'germany', // Germany: Nurnberg 2010, Munich 2011, Hamburg 2012, Berlin 2012, etc.
+	'denmark', // Denmark: Aarhus 2015
+	'spain', // Spain: Zaragoza 2015
+	'france', // France: Isola 2009, Isola 2011, Paris 2011
+	'britain', // United Kingdom: Cambridge 2015, etc: 4 times
+	'greece', // Greece: Rhodes 2012
+	'croatia', // Croatia: Poreč 2011, Plitvice Lakes 2012
+	'hungary', // Hungary
+	'italy', // Italy: Rome 2009, Milan 2013
+	'poland', // Poland: Poznan 2010
+	'romania', // Romania: Cluj-Napoca, etc.
+	'sweden', // Sweden: Malmo 2015
+	'slovenia', // Slovenia: Koper 2011
+	'slovakia', // Slovakia: Krompachy, etc.
+	'turkey', // Turkey: Marmaris 2012
+];
+
+countriesSigla =
+  "AFAXALDZASADAOAIAQAGARAMAWAUATAZBSBHBDBBBYBEBZBJBMBTBOBQBABWBRIOBNBGBFBIKHCMCACVKYCFTDCLCNCXCCCOKMCGCDCKCRCIHRCUCWCYCZDKDJDMDOECEGSVGQEREEETFKFOFJFIFRGFPFTFGAGMGEDEGHGIGRGLGDGPGUGTGGGNGWGYHTHMVAHNHKHUISINIDXZIRIQIEIMILITJMJPJEJOKZKEKIKPKRKWKGLALVLBLSLRLYLILTLUMOMKMGMWMYMVMLMTMHMQMRMUYTMXFMMDMCMNMEMSMAMZMMNANRNPNLNCNZNINENGNUNFMPNOOMPKPWPSPAPGPYPEPHPNPLPTPRQARERORURWBLSHKNLCMFPMVCWSSMSTSASNRSSCSLSGSXSKSISBSOZAGSSSESLKSDSRSJSZSECHSYTWTJTZTHTLTGTKTOTTTNTRTMTCTVUGUAAEGBUSUMUYUZVUVEVNVGVIWFEHYEZMZW";
